303583313ec860c140867556bcee85b6cfd4237c
Adam Crockett <arc1224@gmail.com>
2023-04-13 17:08:41 -0700

    working on mobile styling

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -1,37 +1,51 @@
+'use client';
+import { useState } from 'react';
 import styles from './Navbar.module.scss';
-export default function Navbar() {
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <nav className={styles.navWrapper}>
-      <button
-        className={`animate__animated animate__slideInLeft ${styles.first}`}
-      >
-        Home
-      </button>
-      <button
-        className={`animate__animated animate__slideInLeft ${styles.second}`}
-      >
-        Projects
-      </button>
-      <button
-        className={`animate__animated animate__slideInLeft ${styles.third}`}
-      >
-        Work
-      </button>
-      <button
-        className={`animate__animated animate__slideInLeft ${styles.forth}`}
-      >
-        Skills
-      </button>
-      <button
-        className={`animate__animated animate__slideInLeft ${styles.fifth}`}
-      >
-        Contact
-      </button>
-      <button
-        className={`animate__animated animate__slideInLeft ${styles.sixth}`}
-      >
-        Resume
-      </button>
+      <div className={styles.navContent}>
+        <button
+          className={`${styles.navButton} animate__animated animate__slideInLeft ${styles.first}`}
+        >
+          Home
+        </button>
+        <button
+          className={`${styles.navButton} animate__animated animate__slideInLeft ${styles.second}`}
+        >
+          Projects
+        </button>
+        <button
+          className={`${styles.navButton} animate__animated animate__slideInLeft ${styles.third}`}
+        >
+          Work
+        </button>
+        <button
+          className={`${styles.navButton} animate__animated animate__slideInLeft ${styles.forth}`}
+        >
+          Skills
+        </button>
+        <button
+          className={`${styles.navButton} animate__animated animate__slideInLeft ${styles.fifth}`}
+        >
+          Contact
+        </button>
+        <button
+          className={`${styles.navButton} animate__animated animate__slideInLeft ${styles.sixth}`}
+        >
+          Resume
+        </button>
+      </div>
+      <div className={styles.navToggle}>
+        <button onClick={handleToggle}>Toggle</button>
+      </div>
     </nav>
   );
-}
+};
+
+export default Navbar;
