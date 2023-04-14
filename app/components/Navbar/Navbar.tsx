46468bb5dbd61e66164df48b8fdc4ea7fb9edbf2
Adam Crockett <arc1224@gmail.com>
2023-04-14 15:28:13 -0700

    navbar mobile styling

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -1,37 +1,49 @@
+import Footer from '../Footer/Footer';
 import styles from './Navbar.module.scss';
 export default function Navbar() {
   return (
-    <nav className={styles.navWrapper}>
-      <button
-        className={`animate__animated animate__slideInLeft ${styles.first}`}
+    <nav className={styles.navbar}>
+      <label
+        className={`${styles.hamburgerMenu} animate__animated animate__fadeInDown`}
       >
-        Home
-      </button>
-      <button
-        className={`animate__animated animate__slideInLeft ${styles.second}`}
-      >
-        Projects
-      </button>
-      <button
-        className={`animate__animated animate__slideInLeft ${styles.third}`}
-      >
-        Work
-      </button>
-      <button
-        className={`animate__animated animate__slideInLeft ${styles.forth}`}
-      >
-        Skills
-      </button>
-      <button
-        className={`animate__animated animate__slideInLeft ${styles.fifth}`}
-      >
-        Contact
-      </button>
-      <button
-        className={`animate__animated animate__slideInLeft ${styles.sixth}`}
-      >
-        Resume
-      </button>
+        <input type='checkbox' />
+      </label>
+
+      <ul className={styles.buttonWrapper}>
+        <a
+          className={`animate__animated animate__slideInLeft ${styles.hvrSweepToRight} ${styles.first}`}
+        >
+          Home
+        </a>
+        <a
+          className={`animate__animated animate__slideInLeft ${styles.hvrSweepToRight} ${styles.second}`}
+        >
+          Projects
+        </a>
+        <a
+          className={`animate__animated animate__slideInLeft ${styles.hvrSweepToRight} ${styles.third}`}
+        >
+          Work
+        </a>
+        <a
+          className={`animate__animated animate__slideInLeft ${styles.hvrSweepToRight} ${styles.forth}`}
+        >
+          Skills
+        </a>
+        <a
+          className={`animate__animated animate__slideInLeft ${styles.hvrSweepToRight} ${styles.fifth}`}
+        >
+          Contact
+        </a>
+        <a
+          className={`animate__animated animate__slideInLeft ${styles.hvrSweepToRight} ${styles.sixth}`}
+        >
+          Resume
+        </a>
+        <div className={styles.footerWrapper}>
+          <Footer />
+        </div>
+      </ul>
     </nav>
   );
 }
