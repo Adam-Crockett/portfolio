7ab067cc3c1de0e8b2210ef9080b8c88b1593ed9
Adam Crockett <arc1224@gmail.com>
2023-04-15 00:00:25 -0700

    Added dark mode icon to footer

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -1,8 +1,27 @@
+import Image from 'next/image';
 import styles from './Footer.module.scss';
+import darkMode from '../../../public/dark_mode.png';
 const Footer = () => {
+  // const toggleTheme = document.getElementById('toggle-theme-btn');
+  // // const body = document.body;
+
+  // toggleTheme.addEventListener('click', () => {
+  //   console.log('clicked');
+  // });
   return (
     <footer className={styles.siteFooter}>
-      <button>Theme</button>
+      <button className={styles.hvrSweepToLeft} id='toggle-theme-btn'>
+        <Image
+          className={`${styles.darkModeToggle}`}
+          title='Dark mode toggle'
+          src={darkMode}
+          alt='Dark mode toggle'
+          width={35}
+          height={35}
+          style={{ color: 'red' }}
+          priority
+        />
+      </button>
     </footer>
   );
 };
