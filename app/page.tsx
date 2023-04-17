3478e71c96ed60e82f7dc98cd1c358ae27137ae1
Adam Crockett <arc1224@gmail.com>
2023-04-17 16:57:20 -0700

    Removed unused imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,4 @@
-import Image from 'next/image';
-import { Inter } from 'next/font/google';
-import Navbar from './components/Navbar/Navbar';
-
 import styles from './page.module.scss';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
