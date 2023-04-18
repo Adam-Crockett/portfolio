f1fefef01173a71986b8ce66afad74f20d1cbac0
Adam Crockett <arc1224@gmail.com>
2023-04-18 15:52:15 -0700

    Implemented skills page

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -1,34 +1,116 @@
-import styles from './skills.module.scss';
+import styles from './page.module.scss';
 export default function Skills() {
   return (
     <div className={styles.mainContent}>
-      <section>
-        <title>Languages</title>
-        <ul>
-          <li>JavaScript</li>
-          <li>Python</li>
-          <li>Java</li>
-          <li>CSS</li>
-        </ul>
+      {/* <div
+        className={`${styles.divider} animate__animated animate__fadeIn ${styles.dividerDelay}`}
+      > */}
+      <section className={`${styles.sectionOne} ${styles.sectionContainer}`}>
+        <div
+          className={`${styles.sectionTitleWrapper} animate__animated animate__fadeInDown ${styles.dividerDelay}`}
+        >
+          <h2
+            className={`animate__animated animate__fadeInUp ${styles.titleDelay}`}
+          >
+            Languages
+          </h2>
+        </div>
+        <div>
+          <ul>
+            <li
+              className={`animate__animated animate__fadeInDown ${styles.first}`}
+            >
+              JavaScript
+            </li>
+            <li
+              className={`animate__animated animate__fadeInDown ${styles.second}`}
+            >
+              Python
+            </li>
+            <li
+              className={`animate__animated animate__fadeInDown ${styles.third}`}
+            >
+              Java
+            </li>
+            <li
+              className={`animate__animated animate__fadeInDown ${styles.forth}`}
+            >
+              CSS
+            </li>
+          </ul>
+        </div>
       </section>
-      <section>
-        <title>JS Stack</title>
-        <ul>
-          <li>TypeScript</li>
-          <li>React</li>
-          <li>Next.js</li>
-          <li>Gatsby</li>
-          <li>Node.js</li>
-          <li>Express.js</li>
-        </ul>
+      <section className={`${styles.sectionTwo} ${styles.sectionContainer}`}>
+        <div
+          className={`${styles.sectionTitleWrapper} animate__animated animate__fadeInDown ${styles.dividerDelay}`}
+        >
+          <h2
+            className={`animate__animated animate__fadeInUp ${styles.titleDelay}`}
+          >
+            JS Stack
+          </h2>
+        </div>
+        <div>
+          <ul>
+            <li
+              className={`animate__animated animate__fadeInDown ${styles.first}`}
+            >
+              TypeScript
+            </li>
+            <li
+              className={`animate__animated animate__fadeInDown ${styles.second}`}
+            >
+              React
+            </li>
+            <li
+              className={`animate__animated animate__fadeInDown ${styles.third}`}
+            >
+              Next.js
+            </li>
+            <li
+              className={`animate__animated animate__fadeInDown ${styles.forth}`}
+            >
+              Gatsby
+            </li>
+            <li
+              className={`animate__animated animate__fadeInDown ${styles.fifth}`}
+            >
+              Node.js
+            </li>
+            <li
+              className={`animate__animated animate__fadeInDown ${styles.sixth}`}
+            >
+              Express.js
+            </li>
+          </ul>
+        </div>
       </section>
-      <section>
-        <title>Databases</title>
-        <ul>
-          <li>SQL</li>
-          <li>GraphQL</li>
-        </ul>
+      <section className={`${styles.sectionThree} ${styles.sectionContainer}`}>
+        <div
+          className={`${styles.sectionTitleWrapper} animate__animated animate__fadeInDown ${styles.dividerDelay}`}
+        >
+          <h2
+            className={`animate__animated animate__fadeInUp ${styles.titleDelay}`}
+          >
+            Databases
+          </h2>
+        </div>
+        <div>
+          <ul>
+            <li
+              className={`animate__animated animate__fadeInDown ${styles.first}`}
+            >
+              SQL
+            </li>
+            <li
+              className={`animate__animated animate__fadeInDown ${styles.second}`}
+            >
+              GraphQL
+            </li>
+          </ul>
+        </div>
       </section>
+      {/* </div> */}
     </div>
   );
 }
