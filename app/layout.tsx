3ef5cd96a112877421fc0f6a1a78e02ed6733e5f
Adam Crockett <arc1224@gmail.com>
2023-04-21 16:40:59 -0700

    added line animation to nav and footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
-import './globals.css';
+import './globals.scss';
 import 'animate.css';
 
 export const metadata = {
@@ -16,13 +16,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body>
+      <body className='gradientBackground'>
         <header>
           <Navbar />
+          <div className='verticalLine lineNav' />
         </header>
         <main>{children}</main>
         <div className='footerWrapper'>
           <Footer />
+          <div className='verticalLine moveLineUp lineFoot' />
         </div>
       </body>
     </html>
