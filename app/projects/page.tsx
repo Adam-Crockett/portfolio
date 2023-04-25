35b578ce4056e3a3dc4f4e5a0e2ad4ec004fa199
Adam Crockett <arc1224@gmail.com>
2023-04-25 16:57:42 -0700

    started projects page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,4 +1,25 @@
+import Link from 'next/link';
+import Image from 'next/image';
 import styles from './page.module.scss';
 export default function Projects() {
-  return <></>;
+  return (
+    <div className={styles.mainContent}>
+      <section className={`${styles.sectionOne} ${styles.sectionContainer}`}>
+        <div className={styles.projectCard}>
+          <h2 className={styles.cardTitle}>Power Level: EDH Tool</h2>
+          <Link href='#'>
+            <Image src='' alt='test' />
+          </Link>
+        </div>
+      </section>
+      <section className={`${styles.sectionTwo} ${styles.sectionContainer}`}>
+        <div className={styles.projectCard}>
+          <h2 className={styles.cardTitle}>Memories Application</h2>
+          <Link href='#'>
+            <Image src='' alt='test' />
+          </Link>
+        </div>
+      </section>
+    </div>
+  );
 }
