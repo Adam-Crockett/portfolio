c881de296fde2a59c8f948010038f30a302a850b
Adam Crockett <arc1224@gmail.com>
2023-04-28 15:38:36 -0700

    added links and descriptions to project cards

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -16,22 +16,44 @@ const Projects = () => {
           className={`animate__animated animate__slideInRight ${styles.cardOne} ${styles.projectCard} ${styles.leftSideCard}`}
         >
           <h2 className={styles.cardTitle}>Power Level: EDH Tool</h2>
-          <Link className={styles.projectImageWrapper} href='#'>
+          <Link
+            className={styles.projectImageWrapper}
+            href='http://power-level-client.herokuapp.com/'
+            target='_blank'
+            rel='noopener noreferrer'
+            title='Power Level: EDH Tool'
+          >
             <Image
               className={styles.projectImage}
               src={powerLevelPNG}
-              alt='test'
+              alt='Power Level Screenshot'
             />
           </Link>
           <div className={styles.textContent}>
             <p className={styles.projectDescription}>
-              Description of the project
+              I enjoy creating tier lists in the Magic the Gathering format
+              Commander(EDH). As a result I created Power Level, a full-stack
+              web application that allows users to create a tier list for
+              commanders. The site features a drag and drop implementation,
+              managed state, server and API requests, and cached data.
             </p>
-            <p className={styles.projectTools}>Stack tools here</p>
+            <p className={styles.projectTools}>
+              <p>JavaScript</p>
+              <p className={styles.toolSeparator}>|</p>
+              <p>TypeScript</p>
+              <p className={styles.toolSeparator}>|</p>
+              <p>Next.js</p>
+              <p className={styles.toolSeparator}>|</p>
+              <p>React</p>
+              <p className={styles.toolSeparator}>|</p>
+              <p>MongoDB</p>
+              <p className={styles.toolSeparator}>|</p>
+              <p>Node.js</p>
+            </p>
           </div>
           <div className={styles.cardLinks}>
             <Link
-              href='https://github.com/Adam-Crockett'
+              href='https://github.com/Adam-Crockett/client-edh-tier-list'
               target='_blank'
               rel='noopener noreferrer'
               className={styles.cardLinks}
@@ -55,22 +77,45 @@ const Projects = () => {
           className={`animate__animated animate__slideInLeft ${styles.cardTwo} ${styles.projectCard} ${styles.rightSideCard}`}
         >
           <h2 className={styles.cardTitle}>Memories Application</h2>
-          <Link className={styles.projectImageWrapper} href='#'>
+          <Link
+            className={styles.projectImageWrapper}
+            href='https://mynewmemories.net/#/posts'
+            target='_blank'
+            rel='noopener noreferrer'
+            title='Memories Application'
+          >
             <Image
               className={styles.projectImage}
               src={memoriesPNG}
-              alt='test'
+              alt='Memories Screenshot'
             />
           </Link>
           <div className={styles.textContent}>
             <p className={styles.projectDescription}>
-              Description of the project
+              The Memories Project is a full-stack social media application
+              developed using the MERN stack. The users can authenticate using
+              their email and password or use OAuth with a Google account. The
+              project was developed for scalability, managing user database
+              content, authentication, and creating common social media user
+              interactions.
             </p>
-            <p className={styles.projectTools}>Stack tools here</p>
+            <p className={styles.projectTools}>
+              <p>JavaScript</p>
+              <p className={styles.toolSeparator}>|</p>
+              <p>Express</p>
+              <p className={styles.toolSeparator}>|</p>
+              <p>Redux</p>
+              <p className={styles.toolSeparator}>|</p>
+              <p>React</p>
+              <p className={styles.toolSeparator}>|</p>
+              <p>MongoDB</p>
+              <p className={styles.toolSeparator}>|</p>
+              <p>Node.js</p>
+            </p>
           </div>
           <div className={styles.cardLinks}>
             <Link
-              href='https://github.com/Adam-Crockett'
+              href='https://github.com/Adam-Crockett/memories_project'
               target='_blank'
               rel='noopener noreferrer'
               className={styles.cardLinks}
